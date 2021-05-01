6c289b476f1f13a411158c437333aa1917873168
kazuhisa honda <ringhomomorphism0to0@yahoo.co.jp>
2021-05-01 16:36:23 +0900

    コメント構文 ・タグ外のコメントは通常の文法の通り可能でOK ・タグ内のコメントは、行コメントの場合、{}で括り行末の改行が必要 　または、部分コメント形式で記述 ・<!-- --> は使用できない

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ import './App.css';
 
 function App() {
   return (
+    // タグ外のコメントは通常の文法の通り可能でOK
     <React.Fragment>
+      {// タグ内のコメントは、行コメントの場合、{}で括り行末の改行が必要
+      }
+      {/* または、部分コメント形式で記述 */}
+      {/* ※<!-- --> は使用できない */}
       <p>hello</p>
       <p>world</p>
+      
       <div>
         <label htmlFor="inputForHtmlFor1">htmlFor:&nbsp;</label>
         <input id="inputForHtmlFor1" className="inputForHtmlFor" tabIndex={2} />&nbsp;
