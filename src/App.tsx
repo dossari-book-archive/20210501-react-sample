a03f74cfcdfcab13f48bd3014c0d559e5ec4de65
kazuhisa honda <ringhomomorphism0to0@yahoo.co.jp>
2021-05-01 14:43:18 +0900

    名前の異なる属性 ・for -> htmlFor ・class -> className ・tabindex -> tabIndex など…

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ function App() {
     <React.Fragment>
       <p>hello</p>
       <p>world</p>
+      <div>
+        <label htmlFor="inputForHtmlFor1">htmlFor:&nbsp;</label>
+        <input id="inputForHtmlFor1" className="inputForHtmlFor" tabIndex={2} />&nbsp;
+        <input id="inputForHtmlFor2" className="inputForHtmlFor" tabIndex={1} />
+      </div>
       <img src={logo} />
     </React.Fragment>
   );
